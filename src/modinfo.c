597276f5aeb274da5a7c3a915058572685030333
Irfan Mohammad <irfan.haslanded@gmail.com>
2025-02-08 00:33:22 +0000

    use oper_push_data cache for sr_apply_oper_changes
    The previous commit eb346676a78c015f00707dcfaff03ddf85b79cdb used the
    session cached operational push data for preparing the edit.
    
    We can go further and use the same cache for `sr_apply_oper_changes()`
    as well, further reducing the need to parse and free data trees.

--- src/modinfo.c
+++ src/modinfo.c
@@ -1251,7 +1251,7 @@ sr_oper_data_merge_cb(struct lyd_node *trg_node, const struct lyd_node *src_node
  * @return err_info, NULL on success.
  */
 static sr_error_info_t *
-sr_module_oper_data_load(struct sr_mod_info_mod_s *mod, sr_conn_ctx_t *conn, uint32_t sid,
+sr_module_oper_data_load(struct sr_mod_info_mod_s *mod, sr_conn_ctx_t *conn, sr_session_ctx_t *sess,
         struct lyd_node **mod_oper_data, struct lyd_node **data)
 {
     sr_error_info_t *err_info = NULL;
@@ -1262,6 +1262,7 @@ sr_module_oper_data_load(struct sr_mod_info_mod_s *mod, sr_conn_ctx_t *conn, uin
     struct ly_set *set;
     uint32_t i, j, merge_opts, oper_push_count = 0;
     int last_sid = 0, dead_cid = 0;
+    uint32_t sid = sess ? sess->sid : 0;
 
     /* oper_push_data_count is protected by operational DS module data locks */
     if (mod->shm_mod->oper_push_data_count) {
@@ -1314,8 +1315,13 @@ sr_module_oper_data_load(struct sr_mod_info_mod_s *mod, sr_conn_ctx_t *conn, uin
             last_sid = 1;
         }
         if (!last_sid || !mod_oper_data) {
+            if ((sid == oper_push_dup[i].sid) && sess->oper_push_data) {
+                SR_LOG_DBG("%s.%d consuming cached data for %s", __func__, __LINE__, mod->ly_mod->name);
+                /* load push oper data from the session cache (consume it) */
+                mod_data = sr_module_data_unlink(&sess->oper_push_data, mod->ly_mod, 1);
+            }
             /* load push oper data for the session */
-            if ((err_info = sr_module_file_data_append(mod->ly_mod, mod->ds_handle, SR_DS_OPERATIONAL,
+            else if ((err_info = sr_module_file_data_append(mod->ly_mod, mod->ds_handle, SR_DS_OPERATIONAL,
                     oper_push_dup[i].cid, oper_push_dup[i].sid, NULL, 0, &mod_data))) {
                 goto cleanup;
             }
@@ -1390,7 +1396,7 @@ cleanup:
 }
 
 sr_error_info_t *
-sr_modinfo_get_oper_data(struct sr_mod_info_s *mod_info, uint32_t sid, struct lyd_node **oper_data)
+sr_modinfo_get_oper_data(struct sr_mod_info_s *mod_info, sr_session_ctx_t *sess, struct lyd_node **oper_data)
 {
     sr_error_info_t *err_info = NULL;
     struct sr_mod_info_mod_s *mod;
@@ -1408,7 +1414,7 @@ sr_modinfo_get_oper_data(struct sr_mod_info_s *mod_info, uint32_t sid, struct ly
         }
 
         /* load the requested oper data */
-        if ((err_info = sr_module_oper_data_load(mod, mod_info->conn, sid, oper_data ? &mod_oper_data : NULL,
+        if ((err_info = sr_module_oper_data_load(mod, mod_info->conn, sess, oper_data ? &mod_oper_data : NULL,
                 &mod_info->data))) {
             goto cleanup;
         }
@@ -1456,7 +1462,7 @@ sr_module_oper_data_update(struct sr_mod_info_mod_s *mod, const char *orig_name,
 
     if (!(get_oper_opts & SR_OPER_NO_STORED)) {
         /* process stored operational data */
-        if ((err_info = sr_module_oper_data_load(mod, conn, 0, NULL, data))) {
+        if ((err_info = sr_module_oper_data_load(mod, conn, NULL, NULL, data))) {
             return err_info;
         }
 
@@ -3033,8 +3039,11 @@ sr_modinfo_data_load(struct sr_mod_info_s *mod_info, int read_only, sr_session_c
     /* use cached oper push data if available and usable */
     if ((mod_info->ds == SR_DS_OPERATIONAL) && (mod_info->ds2 == SR_DS_OPERATIONAL) && sess &&
             sess->oper_push_data && !mod_info->data) {
-        mod_info->data = sess->oper_push_data;
-        sess->oper_push_data = NULL;
+        SR_LOG_DBG("%s.%d using cached data", __func__, __LINE__);
+        /* need to duplicate and not consume it, as sr_apply_changes will still need this */
+        if ((err_info = sr_lyd_dup(sess->oper_push_data, NULL, LYD_DUP_RECURSIVE | LYD_DUP_WITH_FLAGS, 1, &mod_info->data))) {
+            goto cleanup;
+        }
         mod_data_load = 0;
     }
 
--- src/sysrepo.c
+++ src/sysrepo.c
@@ -4185,7 +4185,7 @@ sr_apply_oper_changes(struct sr_mod_info_s *mod_info, sr_session_ctx_t *session,
     mod_info->data = NULL;
 
     /* get the operational DS data with the old push oper data */
-    if ((err_info = sr_modinfo_get_oper_data(mod_info, session->sid, NULL))) {
+    if ((err_info = sr_modinfo_get_oper_data(mod_info, session, NULL))) {
         goto cleanup;
     }
 
@@ -4194,7 +4194,7 @@ sr_apply_oper_changes(struct sr_mod_info_s *mod_info, sr_session_ctx_t *session,
     mod_info->data = NULL;
 
     /* get the operational DS data with the new push oper data */
-    if ((err_info = sr_modinfo_get_oper_data(mod_info, session->sid, &new_oper_data))) {
+    if ((err_info = sr_modinfo_get_oper_data(mod_info, session, &new_oper_data))) {
         goto cleanup;
     }
 
@@ -4230,6 +4230,8 @@ sr_apply_oper_changes(struct sr_mod_info_s *mod_info, sr_session_ctx_t *session,
 
     /* cache the newly stored oper push data unless we are deleting the session or cache is disabled */
     if (!shmmod_session_del && !(session->conn->opts & SR_CONN_NO_OPER_PUSH_CACHE)) {
+        /* remove any previous cached oper_push_data and cache the new one */
+        lyd_free_siblings(session->oper_push_data);
         session->oper_push_data = mod_info->data;
         mod_info->data = NULL;
     }
