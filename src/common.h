2d1cc8a9d2dd3fa6813edeb1820d68db2a6d6e28
Irfan Mohammad <irfan.haslanded@gmail.com>
2025-10-28 00:40:21 +0000

    simplify libyang printed context locks and caches
    when various caches are enabled, context lock has become significantly
    complex when a printed context is in use.

--- src/common.h
+++ src/common.h
@@ -538,10 +538,11 @@ sr_error_info_t *sr_path_mod_shm(char **path);
 /**
  * @brief Get the path of the ctx SHM.
  *
+ * @param[in] tmp if a temporary filename should be created.
  * @param[out] path Created path. Should be freed by the caller.
  * @return err_info, NULL on success.
  */
-sr_error_info_t *sr_path_ctx_shm(char **path);
+sr_error_info_t *sr_path_ctx_shm(char **path, int tmp);
 
 /**
  * @brief Get the path of the external SHM.
@@ -1020,39 +1021,6 @@ int sr_schema_mount_changed_oper_data(const struct lyd_node *oper_data);
  * @return err_info on error, NULL on success.
  */
 sr_error_info_t *sr_schema_mount_ds_data_update(sr_conn_ctx_t *conn, struct sr_lycc_ds_data_set_s *data_old);
-
-/**
- * @brief Check the existence of all processes in the array removing any dead ones.
- *
- * @param[in] pids Array of PIDs.
- * @param[in] pid_size Size of @p pids array.
- */
-void sr_pid_array_recover(pid_t *pids, uint32_t pid_size);
-
-/**
- * @brief Update CONTEXT READ LOCK (SHM PIDs) of the operational cache data.
- *
- * @param[in] conn Connection to use.
- * @param[in] oper_cache Operational data cache.
- * @return err_info, NULL on success.
- */
-sr_error_info_t *sr_oper_cache_ctx_lock_update(sr_conn_ctx_t *conn, sr_oper_cache_t *oper_cache);
-
-/**
- * @brief Check the existence of all connections in the array removing any dead ones.
- *
- * @param[in] cids Array of CIDs.
- * @param[in] cid_size Size of @p cids array.
- */
-void sr_cid_array_recover(sr_cid_t *cids, uint32_t cid_size);
-
-/**
- * @brief Update CONTEXT READ LOCK (SHM CIDs) of the operational push cached data.
- *
- * @param[in] conn Connection to use.
- * @return err_info, NULL on success.
- */
-sr_error_info_t *sr_oper_push_cache_ctx_lock_update(sr_conn_ctx_t *conn);
 
 /**
  * @brief Add a new oper cache entry.
